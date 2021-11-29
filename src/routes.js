8cf403dc2c265abd6560bd988a7a8a8b6e7e9eba
Jéssica Cintra <jessica.cintra@gmail.com>
2021-11-29 14:50:22 -0300

    changing route config

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import Trip from './pages/Trip/Trip.vue';
 import Inbox from './pages/Inbox/Inbox.vue';
 import Checkout from './pages/Checkout/Checkout.vue';
 export const routes = [
-    { path: '/', component: Home},
+    { path: '*', component: Home},
     { path: '/Success', component: Success},
     { path: '/Trip', component: Trip},
     { path: '/Inbox', component: Inbox},
